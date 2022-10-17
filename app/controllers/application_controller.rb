6de83aa3305f077594097638dff2be3e49f54ea2
rinatcher <46627353+rinatcher@users.noreply.github.com>
2022-10-17 12:44:36 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@
 class ApplicationController < ActionController::Base
 #   after_action :request_response_logger
 
-  def request_response_logger
-    RequestResponseLog.create(
-      remote_ip: request.remote_ip,
-      request_method: request.method,
-      request_url: request.url,
-      response_status: response.status,
-      response_content_type: response.content_type
-    )
-  end
+#   def request_response_logger
+#     RequestResponseLog.create(
+#       remote_ip: request.remote_ip,
+#       request_method: request.method,
+#       request_url: request.url,
+#       response_status: response.status,
+#       response_content_type: response.content_type
+#     )
+#   end
 end
